30e870f7fe69360f5d8c070513eaf4e2140eac75
Rafael Hernández Pérez <rh9064540@gmail.com>
2024-09-09 14:52:54 -0600

    Logro: Creación de menú y programa de Hola Mundo Recursivo

--- 1_1-proyecto-VS/01-conceptos-basicos/proyectosEDvs/src/edu/mx/uttt/recursividad/TestRecursividad.java
+++ 1_1-proyecto-VS/01-conceptos-basicos/proyectosEDvs/src/edu/mx/uttt/recursividad/TestRecursividad.java
@@ -1,5 +1,54 @@
 package edu.mx.uttt.recursividad;
 
-public class TestRecursividad {
+import javax.swing.JOptionPane;
 
+public class TestRecursividad {
+    public static void main(String[] args) {
+       ejecutar();
+    }
+
+    public static String menu(){
+        String menu = "Menu principal \n" +
+                "1) Método iterativo \n" +
+                "2) Método Recursivo \n" +
+                "3) Salir \n" +
+                "Elige la opción";
+            return JOptionPane.showInputDialog(menu);
+    }
+
+    public static void ejecutar(){
+        boolean sentinel = true;
+        String opcion = "";
+
+        operacion op = new operacion();
+
+        int n = 0;
+        do{
+    
+        opcion = menu();
+
+        switch (opcion) {
+            case "1":
+            n = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor: "));
+            op .setN(n);
+            op.imprimir();
+            break;
+
+                case "2":
+                n = Integer.parseInt(JOptionPane.showInputDialog("Introduce el valor: "));
+                op.setN(n);
+                op.imprimir(op.getN());
+                break;
+
+                    case "3": //Salir
+                    sentinel = false;
+                    JOptionPane.showMessageDialog(null, "I´ll be back");
+                    break;
+        
+            default:
+                JOptionPane.showMessageDialog(null, "Menu no valido");
+        } //Cierra switch
+
+            }while(sentinel); //Cierra 
+    }
 }
---
+++ README.md
@@ -0,0 +1,13 @@
+# Estructura de Datos Aplicadas
+![Imagen de la Estructura de Datos](./img/imagen.jpeg)
+
+En este repositorio se encuentran los códigos de las **estructuras de datos** siguientes:
+
+
+1. Recursividad.
+1. Arreglos (_Vectores_ y _Matrices_).
+1. Listas Enlazadas.
+1. Pilas y Colas.
+1. Arboles Binarios.
+
+Realizado por Rafael Hernández Pérez.
